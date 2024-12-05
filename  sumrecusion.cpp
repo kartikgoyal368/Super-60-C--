12190dc13a95c925af6894e58030fce74248f367
kartikgoyal368 <161934453+kartikgoyal368@users.noreply.github.com>
2024-12-05 15:21:58 +0530

    Intial Commit

---  sumrecusion.cpp
+++  sumrecusion.cpp
@@ -2,5 +2,16 @@
 using namepace std;
 int sum(int n);
 int main() {
-    
+    int n;
+    cout<<"Enter the number :";
+    cin>>n;
+    int sum = sum(n);
+    cout<<"The sum is :"<<sum;
+
+}
+int sum(int n){
+    int n;
+    int sum = 0;
+    sum = sum +n;
+    return sum;
 }
